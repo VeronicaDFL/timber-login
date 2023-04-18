1ea55b0476b72c59de170d366adea292c828c145
Veronica De Felice <verodefelic@gmail.com>
2023-04-18 17:16:27 -0400

    FIRST commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { Button } from "react-bootstrap";
+import Login from "./Login.jsx";
+import Signup from "./Signup.jsx";
+
+export default function Home () {
+    const [isMember, setIsMember] = useState (true);
+
+    return (
+        <>
+        {
+        (isMember)
+       ? <Login/>
+       : <Signup/>
+       
+        }
+        <Button onClick ={() => setIsMember(!isMember)} >Switch Form</Button>
+        </>
+    )
+}
